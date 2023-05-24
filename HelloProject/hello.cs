eec30d53b902e80e5596d39c7ee6728e8082edf3
lknknm <108239558+lknknm@users.noreply.github.com>
2023-05-24 11:37:17 -0300

    Initial Commit

---
+++ HelloProject/hello.cs
@@ -0,0 +1,24 @@
+/*
+@(#)File:           hello.cs
+@(#)Purpose:        Help students with first commit on GitHub using Git.
+@(#)Author:         Lucas Mellone
+@(#)Copyright:      (C) Lucas Mellone 2023 under MIT License
+*/
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Olá! Essa é a lista de contribuintes do curso INF-0991:");
+            Console.WriteLine("Hello! This is the contributors list of the INF-0991 course");
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Hello world! My name is Lucas");
+            // Insira a partir dessa linha conforme o exemplo acima um Hello World com seu nome:
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+## Extecamp First Contributions
+
+Esse projeto foi elaborado com base no repositório [First Contributions](https://github.com/firstcontributions/first-contributions), com o objetivo de ajudar os estudantes do curso Microsoft Technologies da Extecamp (Universidade Estadual de Campinas) em controle de versão, e também como exercício para a manutenção de um repositório simples.
+
+Antes de iniciar, é importante instalar o Git conforme documentação oficial disponível em: https://git-scm.com/downloads ou https://docs.github.com/en/get-started/quickstart/set-up-git
+
+Também é importante realizar a autenticação do Git com o GitHub conforme: [Authenticating with GitHub from Git](https://docs.github.com/en/get-started/quickstart/set-up-git#authenticating-with-github-from-git)
+
+
+
