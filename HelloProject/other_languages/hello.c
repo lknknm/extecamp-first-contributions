8803481eb035c0e4d512aa0b216fb857ace249e6
lknknm <108239558+lknknm@users.noreply.github.com>
2023-05-24 15:25:21 -0300

    feat(Hello): other programming languages included

---
+++ HelloProject/other_languages/hello.c
@@ -0,0 +1,28 @@
+/*
+@(#)File:           hello.c
+@(#)Purpose:        Help students with first commit on GitHub using Git.
+@(#)Author:         Lucas Mellone
+@(#)Copyright:      (C) Lucas Mellone 2023 under MIT License
+*/
+
+#include <stdio.h>
+
+// Output constants
+#define C_GREEN  "\e[32m"
+#define C_WHITE  "\e[37m"
+
+int main(void)
+{
+    printf(C_GREEN);
+    printf("Olá! Essa é a lista de contribuintes do curso INF-0991:\n");
+    printf("Hello! This is the contributors list of the INF-0991 course:\n");
+    printf("\n");
+
+    printf(C_WHITE);
+    printf("Hello world! My name is Lucas\n");
+    // Insira a partir dessa linha conforme o exemplo acima um Hello World com seu nome:
+    // Insert a Hello World with your name following the example above after this line:
+    
+
+   return 0;
+}
---
+++ HelloProject/other_languages/hello.cpp
@@ -0,0 +1,22 @@
+/*
+@(#)File:           hello.cpp
+@(#)Purpose:        Help students with first commit on GitHub using Git.
+@(#)Author:         Lucas Mellone
+@(#)Copyright:      (C) Lucas Mellone 2023 under MIT License
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    system("Color 0A");
+    std::cout << "Olá! Essa é a lista de contribuintes do curso INF-0991";
+    std::cout << "Hello! This is the contributors list of the INF-0991 course";
+
+    system("Color 0F");
+    std::cout << "Hello world! My name is Lucas";
+    // Insira a partir dessa linha conforme o exemplo acima um Hello World com seu nome:
+    // Insert a Hello World with your name following the example above after this line:
+
+    return 0;
+}
